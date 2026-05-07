34131c653d9b618b4516a644b69bb47dfdbb0b3d
omar jandali <omarjandali93@gmail.com>
2026-05-06 18:56:15 -0700

    OJ. fixing issues with updating profile within regustration

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -49,6 +49,7 @@ export const updateProfile = async (
   id: string,
   payload: Partial<Profile>,
 ): Promise<Profile> => {
+  console.log("updateProfile function", payload, id);
   const { data, error } = await supabase
     .from("profiles")
     .update({ ...payload, updated_at: new Date().toISOString() })
